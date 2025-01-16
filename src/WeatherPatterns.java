53581cb1e91636f3076f9b9073578ca3c1813315
Sohum Berry <sohumberry@gmail.com>
2025-01-16 14:57:46 -0800

    Third Log

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * The class WeatherPatterns finds the longest span of days in which
  * each day’s temperature is higher than on the previous day in that sequence.
@@ -8,6 +10,8 @@
 
 public class WeatherPatterns {
     public static int[] temps;
+    // Mapping index in the temperatures array to the length of the streak
+    public static HashMap<Integer, Integer> lengthMap = new HashMap<>();
 
     /**
      * Longest Warming Trend
@@ -16,6 +20,7 @@ public class WeatherPatterns {
      */
     public static int longestWarmingTrend(int[] temperatures) {
         temps = temperatures;
+
         return recursive(temperatures[0], Integer.MIN_VALUE, 0, 0);
     }
 
@@ -26,7 +31,10 @@ public class WeatherPatterns {
 
         int maxStreak = 0;
         for (int i = pos; i < temps.length; i++) {
-            maxStreak = Math.max(maxStreak, recursive(temps[i], nowNum, length+1, i));
+            if (lengthMap.get(i) == null || lengthMap.get(i) < length+1) {
+                lengthMap.put(i, length + 1);
+                maxStreak = Math.max(maxStreak, recursive(temps[i], nowNum, length + 1, i));
+            }
         }
 
         return maxStreak;
